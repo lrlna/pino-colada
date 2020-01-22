8f5963bceaef77b100f7d1d24b294c9206a6e128
Matteo Collina <hello@matteocollina.com>
2020-01-22 11:49:46 +0100

    Updated dependencies, drop old node support (8+ now)

--- example-hapi-pino.js
+++ example-hapi-pino.js
@@ -1,34 +1,29 @@
 'use strict'
 
-var Hapi = require('hapi')
+const Hapi = require('@hapi/hapi')
 
-// Create a server with a host and port
-var server = new Hapi.Server()
-server.connection({
-  host: 'localhost',
-  port: 8080
-})
+async function run () {
+  // Create a server with a host and port
+  const server = Hapi.Server({
+    port: 3000,
+    host: 'localhost'
+  })
 
-// Add the route
-server.route({
-  method: 'GET',
-  path: '/',
-  handler: function (request, reply) {
-    return reply('hello world')
-  }
-})
+  await server.register(require('hapi-pino'))
 
-server.register(require('hapi-pino'), function (err) {
-  if (err) {
-    console.error(err)
-    process.exit(1)
-  }
-
-  // Start the server
-  server.start(function (err) {
-    if (err) {
-      console.error(err)
-      process.exit(1)
+  // Add the route
+  server.route({
+    method: 'GET',
+    path: '/',
+    handler: function (request, reply) {
+      return 'hello world'
     }
   })
+
+  await server.start()
+}
+
+run().catch((err) => {
+  console.error(err)
+  process.exit(1)
 })
